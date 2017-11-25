e10cbfc32b1cfcafce40eb7a134454fa0d3221c2
maccorzo <marko@hankkila.me>
2017-11-25 20:54:34 +0200

    remove StorePicker

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -4,6 +4,4 @@ import { render } from 'react-dom';
 import './css/style.css'
 import App from './components/App';
 
-import StorePicker from './components/StorePicker';
-
 render(<App/>, document.querySelector('#main'));
